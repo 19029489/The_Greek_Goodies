d424c517da844cdbacfe4836b4835d73c1df5b75
19029489 <19029489@rp.edu.sg>
2021-06-30 19:37:43 +0800

    Connected to API

--- app/src/main/java/com/example/thegreekgoodies/MainActivity.java
+++ app/src/main/java/com/example/thegreekgoodies/MainActivity.java
@@ -9,7 +9,18 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.*;
+import com.loopj.android.http.AsyncHttpClient;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +30,27 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager2;
     private Handler sliderHandler = new Handler();
 
+    private AsyncHttpClient client;
+
+    ImageView iv1, iv2, iv3, iv4, iv5, iv6, iv7, iv8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //slider
         viewPager2 = findViewById(R.id.viewPagerImageSlider);
+
+        //image views
+        iv1 = findViewById(R.id.imageView1);
+        iv2 = findViewById(R.id.imageView2);
+        iv3 = findViewById(R.id.imageView3);
+        iv4 = findViewById(R.id.imageView4);
+        iv5 = findViewById(R.id.imageView5);
+        iv6 = findViewById(R.id.imageView6);
+        iv7 = findViewById(R.id.imageView7);
+        iv8 = findViewById(R.id.imageView8);
 
         //Preparing list of images from drawable
         //Can get from API as well
@@ -58,6 +84,52 @@ public class MainActivity extends AppCompatActivity {
                 sliderHandler.postDelayed(sliderRunnable, 5000); //Slide duration 5 seconds
             }
         });
+
+        //Connecting to API
+
+        //collection id number
+        ArrayList<String> collections = new ArrayList<String>();
+        collections.add("160858603610");
+        collections.add("218101055642");
+        collections.add("218101743770");
+        collections.add("218107314330");
+        collections.add("218571571354");
+        collections.add("218459275418");
+        collections.add("218572882074");
+        collections.add("244129726642");
+
+        ArrayList<ImageView> iv = new ArrayList<ImageView>();
+        iv.add(iv1);
+        iv.add(iv2);
+        iv.add(iv3);
+        iv.add(iv4);
+        iv.add(iv5);
+        iv.add(iv6);
+        iv.add(iv7);
+        iv.add(iv8);
+
+        for (int i = 0; i<collections.size(); i++) {
+            client = new AsyncHttpClient();
+            int finalI = i;
+            client.get("https://d10489d24d3e88405b3f523453ff2dca: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@superfoursixty-co.myshopify.com/admin/api/2021-04/collections/" + collections.get(i) + ".json", new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                    try {
+                        Log.i("JSON Results: ", response.toString());
+
+                        String imageURL = response.getJSONObject("collection").getJSONObject("image").getString("src");
+                        Picasso.with(MainActivity.this).load(imageURL).into(iv.get(finalI));
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
+
+
+                }//end onSuccess
+            });
+        }
+
     }
 
     private Runnable sliderRunnable = new Runnable() {
